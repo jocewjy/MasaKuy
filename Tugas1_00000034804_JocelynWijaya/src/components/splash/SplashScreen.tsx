3fbe6ee70dcb64fcc29e7eac74a92d8faac5eaad
Jocelyn Wijaya <74348038+jocewjy@users.noreply.github.com>
2023-10-22 10:47:54 +0700

    Delete Tugas1_00000034804_JocelynWijaya directory

--- Tugas1_00000034804_JocelynWijaya/src/components/splash/SplashScreen.tsx
+++
@@ -1,9 +0,0 @@
-import './SplashScreen.css';
-
-interface SplashScreen {}
-
-const SplashScreen: React.FC<SplashScreen> = () => {
-	return <div className='splash'></div>;
-};
-
-export default SplashScreen;
